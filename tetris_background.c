8f539bef8e62ad7687a0a5eefab25512808913f7
Ahnseojun <70713842+Ahnseojun@users.noreply.github.com>
2022-08-01 15:14:40 +0900

    Next board 수정 (미완성(2))

--- tetris_background.c
+++ tetris_background.c
@@ -3,8 +3,8 @@
 #include<Windows.h>
 
 #define Block_kind 7  // 
-#define Block_size 4  // 4차원 배열에 블럭 모든 형태를 배열로 
-//Block[Bolck_kind][Block_size][Block_size][Block_size]
+#define Block_size 4  // 
+//Block[Bolck_kind][Block_size][Block_size][Block_size];  // Block[7][4][4][4];
 
 #define Board_Width 12
 #define Board_Height 22
@@ -12,13 +12,14 @@
 #define BoardY 0   // Y 위치 - background
 
 
-#define Next_Board_Width 5    // Next_Board
-#define Next_Board_Height 10  // Next_Board
-#define Next_BoardX 60		  // Next_Board
-#define Next_BoardY 0		  // Next_Board
+#define Next_Board_Width 12    // Next_Board
+#define Next_Board_Height 22  // Next_Board
+#define Next_BoardX 45		  // Next_Board
+#define Next_BoardY 2		  // Next_Board
 
 
-void textcolor(int colorNum) { //글자 색 변경
+//글자 색 변경
+void textcolor(int colorNum) { 
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
 }
 
@@ -33,7 +34,7 @@ void GotoXY(int x, int y) { //GotoXY  // cmd창에 x y 좌표 입력
 
 int board[Board_Height][Board_Width]; //board[높이][넓이] 배열
 
-//메인 보드 틀 구성
+//메인 보드 틀 구성  // board[][] 의 값을 만드는 공간
 void creatBoard() {
 	for (int y = 0; y < Board_Height; y++) {     // #define Borad_Height 22
 		board[y][0] = 1;
@@ -55,20 +56,20 @@ void creatBoard() {
 
 //메인 보드 모양 출력
 void printBoard() {
-	for (int x = 1; x < 12; x++) {				// 맨 위 경계선  // x < 12 = '_' 몇번 출력
-		GotoXY(BoardX + x * 2, BoardY + 1);		// x, y 좌표 위치 
+	for (int x = 1; x < Board_Width; x++) {				// 맨 위 경계선  // x < 12 = "_" 몇번 출력
+		GotoXY(BoardX + x * 2, BoardY + 1);		// "_" x, y 좌표 위치   
 		textcolor(14);
 		printf("_");
 	}
 
 	for (int y = 0; y < Board_Height; y++) {	// 왼쪽 벽면 // y++ 이기 때문에 x는 고정 , y가 아래로 내려감
-		GotoXY(BoardX, BoardY + y);
-		if (board[y][0] == 1) {
+		GotoXY(BoardX, BoardY + y);				// BoardY + y(++)를 해서 아래로 출력  // x,y 좌표
+		if (board[y][0] == 1) {					// board[Board_Height][Board_Width];
 			textcolor(5);
 			printf("■");
 		}
 		if (board[y][Board_Width - 1] == 1) {	// 오른쪽 벽면
-			GotoXY(BoardX + Board_Width * 2 - 2, BoardY + y);		// 오른쪽 벽 x,y 좌표
+			GotoXY(BoardX + Board_Width * 2 -2, BoardY + y);		// 오른쪽 벽 x,y 좌표
 			textcolor(5);
 			printf("■");
 		}		
@@ -82,49 +83,56 @@ void printBoard() {
 		}
 	}
 	printf("\n");
-	textcolor(10); // 이상한 글자 색 변경 <- 이 창을 닫으려면..
+	textcolor(10); // 글자 색 변경 <- 이 창을 닫으려면..
 }
 
 
-// Next 블럭 표시칸 - 미완성 //
+// Next 블럭 표시칸 - 미완성(2) //
 void print_Next_Board() {
-	for (int x = 1; x < 14; x++) {	// 맨 위 경계선
-		GotoXY(Next_BoardX + x * 2, Next_BoardY + 1);
+		// 맨 위 경계선 next 출력
+		GotoXY(Next_BoardX + 0 * 2, Next_BoardY );
 		textcolor(14);
-		printf("■");
-	}
+		printf("< N E X T >");
 
-	for (int y = 0; y < Board_Height; y++) {	// 왼쪽 벽면
+	for (int x = 1; x < Next_Board_Width; x++) {	// 맨 위 벽
+		GotoXY(Next_BoardX + x * 2, Next_BoardY + 1);
+		textcolor(4);
+		printf("■");
+	}	
+
+	for (int y = 0; y < 7; y++) {					// 왼쪽 벽면  // y < Next_Board_Height
 		GotoXY(Next_BoardX, Next_BoardY + y);
 		if (board[y][0] == 1) {
-			textcolor(5);
+			textcolor(4);
 			printf("■");
 		}
-		if (board[y][Board_Width - 1] == 1) {	// 오른쪽 벽면
-			GotoXY(Next_BoardX + Board_Width * 2 - 2, Next_BoardY + y);
-			textcolor(5);
+
+		if (board[y][Next_Board_Width - 1] == 1) {	// 오른쪽 벽면
+			GotoXY(Next_BoardX + Next_Board_Width * 2 - 2 , Next_BoardY + y);
+			textcolor(4);
 			printf("■");
 		}
 	}
 
-	for (int x = 0; x < Board_Width; x++) { //맨 밑 벽
-		GotoXY(Next_BoardX + x * 2, Next_BoardY + Board_Height - 1);
-		if (board[Board_Height - 1][x] == 1) {
-			textcolor(5);
+	for (int x = 0; x < Next_Board_Width; x++) {	//맨 밑 벽
+		GotoXY(Next_BoardX + x * 2, Next_BoardY + 8 - 1);	// Next_Board_Height - 1
+		if (board[Next_Board_Height - 1][x] == 1) {
+			textcolor(4);
 			printf("■");
 		}
 	}
-	textcolor(10); // 이상한 글자 색 변경 <- 이 창을 닫으려면..
+	printf("\n");
+	textcolor(10); // 글자 색 변경 <- 이 창을 닫으려면..
 }
-
 
 
 
 int main() {
+	
+	creatBoard();		// 메인보드 틀 불러오기
+	print_Next_Board(); // Next_Board 출력
 
-	creatBoard(); // 메인보드 틀 불러오기
-	printBoard(); // 메인보드 출력
-
+	printBoard();		// 메인보드 출력
 
 	return 0;
 }
